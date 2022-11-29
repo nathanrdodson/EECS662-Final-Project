5c69f0361e083e6cad656aa30d35a424010615bd
TristanNiles <tristannilesmcculloch@gmail.com>
2022-11-29 17:28:48 -0600

    finished typeof

--- final_project.hs
+++ final_project.hs
@@ -34,6 +34,7 @@ typeof g (Num x) =
     if x < 0
     then Nothing
     else return TNum
+typeof g (Boolean x) = Just (TBool)
 typeof g (Id x) = (lookup x g)
 typeof g (Plus x y) = do {
     TNum <- typeof g x;
@@ -59,8 +60,44 @@ typeof g (Lambda i d b) = do {
     r <- typeof ((i,d):g) b;
         return (d :->: r)
 }
-
-typeof _ _ = Nothing
+typeof g (App x y) = do {
+    y' <- typeof g y;
+    d :->: r <- typeof g x;
+        if y' == d
+        then return r
+        else Nothing
+}
+typeof g (Bind x y z) = do {
+    y' <- typeof g y;
+        typeof ((x,y'):g) z
+}
+typeof g (If x y z) = do {
+    TBool <- typeof g x;
+    y' <- typeof g y;
+    z' <- typeof g z;
+        if y' == z'
+        then return y'
+        else Nothing
+}
+typeof g (And x y) = do {
+    TBool <- typeof g x;
+    TBool <- typeof g y;
+        return TBool
+}
+typeof g (Or x y) = do {
+    TBool <- typeof g x;
+    TBool <- typeof g y;
+        return TBool
+}
+typeof g (Leq x y) = do {
+    TNum <- typeof g x;
+    TNum <- typeof g y;
+        return TBool
+}
+typeof g (IsZero x) = do {
+    TNum <- typeof g x;
+        return TBool
+}
 
 eval :: Env -> FINLANG -> (Maybe FINLANG)
 eval _ _ = Nothing
